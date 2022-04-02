f1c209f7f85aa96574b8d7e1beda9b00f2ecae85
stratis_miritzis <stratismiritzis@gmail.com>
2022-04-02 14:40:39 +0300

    fixed 7

--- src/main/java/se_project/entity/AccountId.java
+++ src/main/java/se_project/entity/AccountId.java
@@ -3,9 +3,17 @@ package se_project.entity;
 import java.io.Serializable;
 
 public class AccountId implements Serializable {
-    private int id;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 271767987177583490L;
+
+	private int id;
 
     private int courseId;
+
+	public AccountId() {
+	}
 
 	public AccountId(int id, int courseId) {
 		super();
@@ -13,6 +21,32 @@ public class AccountId implements Serializable {
 		this.courseId = courseId;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + courseId;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountId other = (AccountId) obj;
+		if (courseId != other.courseId)
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
     // default constructor
 
 }
